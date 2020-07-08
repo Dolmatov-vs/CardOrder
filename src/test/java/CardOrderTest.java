08791ee12ad581ba62e39ffed4cf68504ca6962f
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-07-08 17:52:54 +0300

    edit test

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -12,26 +12,25 @@ public class CardOrderTest {
     void setUp() {
         open("http://localhost:9999");
         $("[data-test-id='name']").shouldHave(text("Укажите точно как в паспорте"));
-        $$(By.className("input__sub")).get(1).shouldHave(text("На указанный номер моб. тел. будет отправлен смс-код для подтверждения заявки на карту. Проверьте, что номер ваш и введен корректно."));
+        $("[data-test-id='phone']").shouldHave(text("На указанный номер моб. тел. будет отправлен смс-код для подтверждения заявки на карту. Проверьте, что номер ваш и введен корректно."));
         $("[class='input__sub']").shouldHave(cssValue("color", "rgba(11, 31, 53, 0.6)"));
-        $$(By.className("input__sub")).get(1).shouldHave(cssValue("color", "rgba(11, 31, 53, 0.6)"));
     }
 
     @Test
     void shouldApplicationSentSuccessfully() {
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79281234567");
-        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[class='Success_successBlock__2L3Cw']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldNameAndSurnameEnteredIncorrectly() {
         $("[data-test-id='name'] input").setValue("Ivanov Ivan");
         $("[data-test-id='phone'] input").setValue("+79281234567");
-        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id=agreement]").click();
+        $("button").click();
         $("[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
         $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
@@ -40,9 +39,9 @@ public class CardOrderTest {
     void shouldTelephoneEnteredIncorrectly() {
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+7928123456");
-        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $(By.className("input_invalid")).shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
         $("[data-test-id='phone']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
@@ -50,9 +49,9 @@ public class CardOrderTest {
     void shouldTelephoneAndNameEnteredIncorrectly() {
         $("[data-test-id='name'] input").setValue("Ivanov Ivan");
         $("[data-test-id='phone'] input").setValue("+7928123456");
-        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $(By.className("input_invalid")).shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
         $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 }
