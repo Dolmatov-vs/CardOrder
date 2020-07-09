db5d79354d6b03b85566456b58db8678d1118c8d
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-07-09 12:36:06 +0300

    edit test

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,7 +1,6 @@
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -21,7 +20,7 @@ public class CardOrderTest {
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79281234567");
         $("[data-test-id=agreement]").click();
-        $("button").click();
+        $(".form button").click();
         $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
@@ -30,9 +29,9 @@ public class CardOrderTest {
         $("[data-test-id='name'] input").setValue("Ivanov Ivan");
         $("[data-test-id='phone'] input").setValue("+79281234567");
         $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(".form button").click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
@@ -40,9 +39,9 @@ public class CardOrderTest {
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+7928123456");
         $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        $("[data-test-id='phone']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(".form button").click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_invalid[data-test-id='phone']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
@@ -50,25 +49,25 @@ public class CardOrderTest {
         $("[data-test-id='name'] input").setValue("Ivanov Ivan");
         $("[data-test-id='phone'] input").setValue("+7928123456");
         $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(".form button").click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
     void shouldApplicationSentNoChecbox() {
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79281234567");
-        $("button").click();
-        $("[class='checkbox__text']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(".form button").click();
+        $(".input_invalid[data-test-id=agreement]").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
     void shouldNotRequiredFieldWhenSending() {
         $("[data-test-id='phone'] input").setValue("+79281234567");
         $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id='name']").shouldHave(text("Поле обязательно для заполнения"));
-        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(".form button").click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Поле обязательно для заполнения"));
+        $(".input_invalid[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 }
